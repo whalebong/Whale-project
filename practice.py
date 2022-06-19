27067af8799e4f1bea5f3dd9857e3f3454d2bab4
Bongguen KIM <sksqhdrms87@naver.com>
2022-06-19 17:59:30 +0900

    time

--- practice.py
+++ practice.py
@@ -1,5 +1,11 @@
-absent = [2, 5]
-for student in range(1, 11):
-    if student in absent:
-        continue
-    print("{0}, 책을 읽어봐".format(student))
+from random import *
+cnt = 0
+for i in range(1, 51):
+    time = randrange(5, 51)
+    if 5 <= time <= 15:
+        print("[O] {0}번째 손님 (소요시간 : {1}분)".format(i, time))
+        cnt += 1
+    else:
+        print("[ ] {0}번째 손님 (소요시간 : {1}분)".format(i, time))
+    
+    print("총 탑승 승객 : {0} 분".format(cnt))
